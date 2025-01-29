c0e1f473610adbb3dd3989ff0ba49c9ff64a7fa1
em71gg <em71gg@gmail.com>
2025-01-29 21:08:02 +0100

    primer commit laravel

---
+++ DWS/prolav/prolavavel/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/second', function () {
+    return view('second');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
