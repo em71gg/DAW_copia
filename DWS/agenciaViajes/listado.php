08eb9b9a4fa5920e63ad5df9856e9ab2596c0131
em71gg <em71gg@gmail.com>
2025-02-05 21:37:29 +0100

    cmmint fijnal calse 05 febrero

--- DWS/agenciaViajes/listado.php
+++ DWS/agenciaViajes/listado.php
@@ -1,6 +1,13 @@
 <?php
-require_once('../utiles/');
-require_once('../utiles/')
+require_once('./utiles/funciones.php');
+require_once('./utiles/variables.php');
+
+$conexion = conectarPDO($host, $user, $password, $bbdd);
+
+$consulta = $conexion->prepare("SELECT nombre, descripcion, fecha_actividad, aforo FROM ofertas WHERE visada=1");
+$consulta->execute();
+
+$rdo = $consulta -> fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
@@ -21,8 +28,8 @@ require_once('../utiles/')
             <div class="header-container">
                 <div></div>
                 <div>
-                    <p class="header-title">Aplicación Empresa</p>
-                </div>   
+                    <p class="header-title">Agencia de viajes</p>
+                </div>
                 <div id="options-header">
                     <form action="#" method="post">
                         <div class="logout-button-container">
@@ -41,59 +48,29 @@ require_once('../utiles/')
     <main id="content" role="main">
         <div class="table-container">
             <div class=info-table>
-        </div>
-        <div class="form-container"> 
-            <div class="form-card">
                 <div class="text-center">
-                    <h1 class="title">¿Olvidó su contraseña?</h1>
-                    <p class="text-sm">
-                        ¿Recuerda su contraseña?
-                        <a class="link" href="../login.php">Ir a login</a>
-                    </p>
+                    <h1 class="title">Listado de actividades</h1>
                 </div>
-                <div class="form-section">
-                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-                        <div class="form-group">
-                            <label for="email" class="label">Dirección email</label>
-                            <input type="text" id="email" name="email" class="input" placeholder="<?php echo ($email != "" ? $email : "Email") ?>" />
-                        </div>
-                        <div class="text-center">
-                            <?php if (!empty($errores['noUsuario'])): ?>
-                                <p class="error"><?php echo $errores['noUsuario'] ?></p>
-                            <?php endif; ?>
-                            <?php if (!empty($errores['emailVacio'])): ?>
-                                <p class="error"><?php echo $errores['emailVacio'] ?></p>
-                            <?php endif; ?>
-                            <?php if (!empty($errores['errorEmail'])): ?>
-                                <p class="error"><?php echo $errores['errorEmail'] ?></p>
-                            <?php endif; ?>
-                        </div>
-                        <button type="submit" class="btn">Reset password</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="info-container">
-            <div class="info-card">
-                <div class="text-left">
-                    <ul class="info-list">
-                        <li>
-                            <p class="info-text">Se confirma el correo, si está registrado se ingresa la nueva contraseña.</p>
-                        </li>
-                        <li>
-                            <p class="info-text">Se valida la contraseña, se genera un token y se inactiva al usuario.</p>
-                        </li>
-                        <li>
-                            <p class="info-text">El usuario recibe un correo con un link de confirmación.</p>
-                        </li>
-                        <li>
-                            <p class="info-text">Tendrá 5 minutos para usarlo.</p>
-                        </li>
-                        <li>
-                            <p class="info-text">Al pulsar el link de confirmación cambia la contraseña, se activa el usuario y se borra el token.</p>
-                        </li>
-                    </ul>
-                </div>
+                <table id="customers">
+                    <tr>
+                        <th>Nombre de la Actividad</th>
+                        <th>Descripción de la actividad</th>
+                        <th>Fecha de la actividad</th>
+                        <th>Aforo</th>
+                    </tr>
+                    <?php
+
+                    foreach($rdo as $campo){
+                        echo "<tr>
+                        <td>" . $campo['nombre'] . "</td>
+                        <td>" . $campo['descripcion'] . "</td>
+                        <td>" . $campo['fecha_actividad'] . "</td>
+                        <td>" . $campo['aforo'] . "</td>
+                    </tr>".PHP_EOL;
+                    }
+                   
+
+                    ?>
             </div>
         </div>
     </main>
